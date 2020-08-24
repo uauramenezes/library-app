dfa5bf958227bd546eb295f313c1f7953261fdc3
Uaurá Menezes <jouchark@gmail.com>
2020-08-24 18:59:23 -0300

    add read status option

--- script/script.js
+++ script/script.js
@@ -10,6 +10,7 @@ function addBookToLibrary() {
     this.title = document.getElementById('title').value;
     this.author = document.getElementById('author').value;
     this.pages = document.getElementById('pages').value;
+    this.readStatus = document.getElementById('readStatus').checked
 }
 
 let addBook = document.getElementById('addBook')
@@ -56,7 +57,8 @@ function resetForm() {
 function showBook() {
     createDiv();
     makeTemplate();
-    createDeleteButton()
+    createReadButton();
+    createDeleteButton();
 }
 
 function createDiv() {
@@ -78,21 +80,43 @@ function makeTemplate() {
     }
 }
 
+function createReadButton() {
+    let readButton = document.createElement('input');
+    readButton.type = 'button';
+    readButton.value = readNotRead(readButton)
+    div.appendChild(readButton);
+}
+
+function readNotRead(readButton) {
+    if (readStatus.checked) {
+        readButton.value = 'Read';
+    } else  {
+        readButton.value = 'Not Read';
+    }
+    readButton.onclick = function() {
+        if (readButton.value === 'Read') {
+            readButton.value = 'Not Read'
+        } else {
+            readButton.value = 'Read'
+        }
+    }
+    return readButton.value;
+}
+
 function createDeleteButton() {
     let delBtn = document.createElement('input');
     delBtn.type = 'button';
     delBtn.value = 'del';
-    delBtn.className = 'deleteButton'
     delBtn.onclick = function() {
-        deleteBook(delBtn.parentNode.id)
-        return false
+        deleteBook(delBtn.parentNode.id);
+        return false;
     }
     div.appendChild(delBtn);
 }
 
 function deleteBook(id) {
-    let index = id - 1
-    myLibrary.splice(index, 1)
-    let book = document.getElementById(id)
-    book.parentNode.removeChild(book)
+    let index = id - 1;
+    myLibrary.splice(index, 1);
+    let book = document.getElementById(id);
+    book.parentNode.removeChild(book);
 }
