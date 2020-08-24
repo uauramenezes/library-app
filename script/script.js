a17036a308ca4618e5083d26b7b0fbef53d677a7
Uaurá Menezes <jouchark@gmail.com>
2020-08-24 18:18:57 -0300

    add button to delete book

--- script/script.js
+++ script/script.js
@@ -7,14 +7,14 @@ function Book(title, author, pages) {
 }
 
 function addBookToLibrary() {
-    this.title = document.getElementById('title').value
-    this.author = document.getElementById('author').value
-    this.pages = document.getElementById('pages').value
+    this.title = document.getElementById('title').value;
+    this.author = document.getElementById('author').value;
+    this.pages = document.getElementById('pages').value;
 }
 
 let addBook = document.getElementById('addBook')
 addBook.addEventListener('click', function () {
-    displayForm(1)
+    displayForm(1);
 })
 
 let submitButton = document.getElementById('submit');
@@ -22,57 +22,77 @@ submitButton.addEventListener('click', function() {
     if (title.value == '' || author.value == '' || pages.value == '') {
         return false
     } else {
-        pushInput()
-        displayForm(2)
-        showBook()
-        resetForm()
+        pushInput();
+        displayForm(2);
+        showBook();
+        resetForm();
     }
 })
 
 let cancelButton = document.getElementById('cancel');
 cancelButton.addEventListener('click', function() {
-    displayForm(2)
-    resetForm()
+    displayForm(2);
+    resetForm();
 })
 
 function pushInput() {
-    let book = new addBookToLibrary()
-    myLibrary.push(book)
+    let book = new addBookToLibrary();
+    myLibrary.push(book);
 }
 
 function displayForm(x) {
     if (x === 1) {
-        document.getElementById('form').style.display = 'block'
+        document.getElementById('form').style.display = 'block';
     } else {
-        document.getElementById('form').style.display = 'none'
+        document.getElementById('form').style.display = 'none';
     }
 }
 
 function resetForm() {
     let frm = document.getElementById('form');
-    frm.reset()
-}
-
-let render = function (template, book) {
-    book.innerHTML = template
+    frm.reset();
 }
 
 function showBook() {
-    createDiv()
-    makeTemplate()
+    createDiv();
+    makeTemplate();
+    createDeleteButton()
 }
 
 function createDiv() {
     let library = document.getElementById('library')
-    book = document.createElement('div');
-    book.className = 'book';
-    library.appendChild(book); 
-    book.id = myLibrary.length
+    div = document.createElement('div');
+    library.appendChild(div); 
+    div.id = myLibrary.length;
+    div.className = 'book';
 };
+
+let render = function (template, book) {
+    book.innerHTML = template;
+}
 
 function makeTemplate() {
     for (i = 0; i < myLibrary.length; i++) {
-        let template = `<h3>${myLibrary[i].title} by ${myLibrary[i].author} has ${myLibrary[i].pages} pages.</h3>`
-        render(template, book)
+        let template = `<h3>${myLibrary[i].title} by ${myLibrary[i].author} has ${myLibrary[i].pages} pages.</h3>`;
+        render(template, div);
     }
+}
+
+function createDeleteButton() {
+    let delBtn = document.createElement('input');
+    delBtn.type = 'button';
+    delBtn.value = 'del';
+    delBtn.className = 'deleteButton'
+    delBtn.onclick = function() {
+        deleteBook(delBtn.parentNode.id)
+        return false
+    }
+    div.appendChild(delBtn);
+}
+
+function deleteBook(id) {
+    let index = id - 1
+    myLibrary.splice(index, 1)
+    let book = document.getElementById(id)
+    book.parentNode.removeChild(book)
 }
