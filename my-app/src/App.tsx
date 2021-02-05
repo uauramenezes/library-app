67bf772aee02e1822d3e060696e0188517aba91d
uauramenezes <uaurasilva@hotmail.com>
2021-02-05 10:38:33 -0300

    Add navbar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="#home">My App</Navbar.Brand>
+            <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">About</Nav.Link>
+            <Nav.Link href="#pricing">Library</Nav.Link>
+            </Nav>
+            <Form inline>
+            <Button variant="outline-info" className='mr-sm-3'>Sign In</Button>
+            <Button variant="outline-info">Sign Up</Button>
+            </Form>
+        </Navbar>
     </div>
   );
 }
