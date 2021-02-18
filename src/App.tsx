ac955eb45e651823896c72a218126ae9a7965975
uauramenezes <uaurasilva@hotmail.com>
2021-02-18 17:34:44 -0300

    gh-pages deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import './App.css';
 import './Authorization/Authorization.css';
 
 import Search from './Search/Search';
 import NavBar from './Navbar/Navbar';
-import SignIn from './Authorization/SignIn';
-import SignUp from './Authorization/SignUp';
 
 function App() {
 
   return (
     <div className="App">
         <NavBar />
-        <Router>
-            <Route exact path='/'> <Search /> </Route>
-            <Route path='/sign-in'> <SignIn /> </Route>
-            <Route path='/sign-up'> <SignUp /> </Route>
-        </Router>
+        <Search />
     </div>
   );
 }
--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -13,19 +13,19 @@ function NavItems() {
     if (width < 450) {
         return(
             <NavDropdown title="Library App" id="basic-nav-dropdown" className='drop-down'>
-                <NavDropdown.Item href="/">Home</NavDropdown.Item>
-                <NavDropdown.Item href="/" disabled>My Library</NavDropdown.Item>
-                <NavDropdown.Item href="/" disabled>Account</NavDropdown.Item>
+                <NavDropdown.Item href="#">Home</NavDropdown.Item>
+                <NavDropdown.Item href="#" disabled>My Library</NavDropdown.Item>
+                <NavDropdown.Item href="#" disabled>Account</NavDropdown.Item>
             </NavDropdown>
         )
     } else {
         return(
             <React.Fragment>
-                <Navbar.Brand href="/" className='site-title'>Library App</Navbar.Brand>
+                <Navbar.Brand href="#" className='site-title'>Library App</Navbar.Brand>
                 <Nav className="mr-auto" id='nav'>
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/" disabled>My Library</Nav.Link>
-                    <Nav.Link href="/" disabled>Account</Nav.Link>
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#" disabled>My Library</Nav.Link>
+                    <Nav.Link href="#" disabled>Account</Nav.Link>
                 </Nav>
             </React.Fragment>
         )
@@ -37,10 +37,10 @@ export default function NavBar() {
         <Navbar bg="dark" variant="dark">
             <NavItems />
             <Form inline>
-                <Button variant="outline-info" className='mr-sm-2 button' href="/sign-in">
+                <Button variant="outline-info" className='mr-sm-2 button' href="#">
                     Sign In
                 </Button>
-                <Button variant="outline-info" className='button' href='sign-up'>Sign Up</Button>
+                <Button variant="outline-info" className='button' href='#'>Sign Up</Button>
             </Form>
         </Navbar>
     )
